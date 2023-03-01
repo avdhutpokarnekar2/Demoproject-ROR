92ad211497b9fe0a96308eeea5b3e1f862c4a20d
Avdhut Pokarnekar <apokarnekar@gmail.com>
2023-03-01 20:23:31 +0530

    front end change in all pages

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -11,23 +11,6 @@ class ProfileController < ApplicationController
     end
   end
 
-  #for subscribe the user
-  def mailchimp
-    mailchimp = MailchimpMarketing::Client.new
-    mailchimp.set_config({
-      :api_key => "bb7c4fc91c190c3c0285b16bbfa69a9c",
-      :server => "us21"
-      })
-    list_id = "2f712a62e2"
-    response = mailchimp.lists.add_list_member list_id,
-                { email_address: params[:email],
-                  status: "subscribed"
-                }
-    redirect_to root_url, notice: "Subscribed"
-    rescue MailchimpMarketing::ApiError => e
-      redirect_to root_url, alert: "This email is already in mailchimp list."
-  end
-
 private
 
   def user_params
